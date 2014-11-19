7cdc5bc208ccc2a097b7b2b86575b1fab715bd8b
Josh Martin <contact@joshmartin.eu>
2014-11-19 03:43:14 +0000

    Enemy movement to middle fix

--- js/objects/enemy.js
+++ js/objects/enemy.js
@@ -3,20 +3,18 @@ function Enemy(sprite, posX, posY){
 	this.sprite = sprite;
 	this.aabb = new AABB(this.sprite);
 	this.speed = 1;
-		
+	this.hp = 100;
+	
 	//Position setting
 	this.posX = posX
 	this.posY = posY
-	this.dir = !Boolean(posX);
-	
 	
 	this.update = function() {
-		if (this.dir){
+		if (this.posX < 480){
 			this.posX+=this.speed;
 		} else {
 			this.posX-=this.speed;
 		}
-		console.log(this.posX);
 	}
 }
 	
